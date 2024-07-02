650221dc8acf3075bac13cdc8de6af6a3e3559c5
Emily Burch <burch.emilys@gmail.com>
2024-07-02 13:46:50 -0400

    Set main scene

--- iOS_Web.service.worker.js
+++ iOS_Web.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1719940771|5151270148";
+const CACHE_VERSION = "1719942304|6683862485";
 const CACHE_PREFIX = "iOS_Web-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "iOS_Web.offline.html";
